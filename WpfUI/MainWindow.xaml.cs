06d575a85d165f0bfd2da24b76921f7d5144e2ba
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-07 18:40:53 -0400

    Initial commit and final
    - Creating a Custom Vision project
    - Trainging the AI using some pictures
    - Sending a request to the REST service
    - Reading the response as JSON
    - Deserializing JSON using (Newtonsoft.Json) package
    - Displaying the result using a GridView

---
+++ WpfUI/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfUI.Classes;
+
+namespace WpfUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Filter = "Image files (*.png; *.jpg) | *.png;*.jpg;*.jpeg|All files (*.*)|*.*";
+            //dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            dialog.InitialDirectory = "D:\\Resources-REST-and-AI-Training-the-AI\\WPF-1040-TrainAI-resources";
+
+            if (dialog.ShowDialog() == true)
+            {
+                string fileName = dialog.FileName;
+
+                selectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                MakePredictionAsync(fileName);
+            }
+        }
+
+        private async void MakePredictionAsync(string fileName)
+        {
+            string url = "Custom Vision Azure Url";
+            string predictionKey = "something";
+            string contentType = "something";
+
+            var file = File.ReadAllBytes(fileName);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Prediction-Key", predictionKey);
+                using (var content = new ByteArrayContent(file))
+                {
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(contentType);
+                    var response = await client.PostAsync(url, content);
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    List<Prediction> predictions = (JsonConvert.DeserializeObject<CustomVision>(responseString)).Predictions;
+
+                    predictionsListView.ItemsSource = predictions;
+                }
+            }
+        }
+    }
+}
